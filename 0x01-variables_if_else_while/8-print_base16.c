124524ca7d5da34907b4474e5d39f0d1c2721d5c
Rosejmwangi <rozzymwangi73@gmail.com>
2023-02-17 15:08:29 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ */
+
+int main(void)
+{
+        char c;
+
+	int d;
+
+	c = 'a';
+	while
+		(d < 10) {
+			putchar(d + '0');
+			d++
+		}
+	while
+		(C <= 'f') {
+			putchar(c);
+			c++
+		}
+	putchar('\n');
+	return (0);
+}
+
