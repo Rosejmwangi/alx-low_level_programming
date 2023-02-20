9f663d132d20925e30d747a7e58cff91cb6bf0ba
Rosejmwangi <rozzymwangi73@gmail.com>
2023-02-20 03:46:35 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - entry point
- *
+ * Description- prints al single numbers
  * return: always 0 success
  */
 
@@ -9,7 +9,7 @@ int main(void)
 {
 	int a;
 
-	for (a = 0; a <= 10; a++)
+	for (a = 0; a <= 9; a++)
 		putchar(a);
 	putchar('\n');
 	return (0);
