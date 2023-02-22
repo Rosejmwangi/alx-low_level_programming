d18a305fc9cb06eee752b3066d9cfcef64c37101
Rosejmwangi <rozzymwangi73@gmail.com>
2023-02-22 10:34:52 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * Entry point of a program that computed the avbsolute value of an integer
+ * @n: integer to check
+ * Return: the absolute value of an integer
+ */
+int _abs(int)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else	
+		return (-n);
+}
