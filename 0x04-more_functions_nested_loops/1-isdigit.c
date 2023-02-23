05aa5bd266f42594f37d8e45c84d2528d204a0ae
Rosejmwangi <rozzymwangi73@gmail.com>
2023-02-23 03:54:22 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ * entry point for a program that checks for a digit (0 through 9)
+ * @c: input of the program
+ * Return 1 if c is a digit otherwise return 0
+ */
+int _isdigit(int c)
