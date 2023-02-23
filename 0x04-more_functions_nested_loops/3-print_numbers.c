cd8759df3814dec91acac342b008460358ecf26d
Rosejmwangi <rozzymwangi73@gmail.com>
2023-02-23 05:07:29 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * Entry point of a program that prints the nymber from 0 to 9 followed by a new line
+ *
+ * Return 0 to 9 followed by a new line
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+_putchar(i + '0');
+_putchar('\n');
+}
+
