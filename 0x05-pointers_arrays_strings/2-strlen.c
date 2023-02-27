68fa16661d584ea8becb95c55432192b39a26fac
Rosejmwangi <rozzymwangi73@gmail.com>
2023-02-27 04:12:42 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * entry point to a function that returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (long);
+}
